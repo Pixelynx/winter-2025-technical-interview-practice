7c37e394d686bdb3153f6833bc6fd70f97b0ef48
pixelynx <tjstevens1992@gmail.com>
2025-01-27 07:25:39 -0500

    string problem.js solution

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -4,8 +4,27 @@
  * @param {string} s
  * @return {number}
  */
+
 function lengthOfLongestSubstring(s) {
-  // Your code here
+  let maxLength = 0;
+
+  for(let i = 0; i < s.length; i++) {
+    let seen = new Set();
+    let currLen = 0;
+
+    for(let j = i; j < s.length; j++) {
+      if(seen.has(s[j])) {
+        break;
+      }
+      seen.add(s[j]);
+      currLen++;
+    }
+
+    maxLength = Math.max(maxLength, currLen);
+  }
+
+  return maxLength;
 }
+
 
 module.exports = lengthOfLongestSubstring;
