c35d9e2593bc42f05d2724d7571dcdc3d575df37
pixelynx <tjstevens1992@gmail.com>
2025-01-29 11:17:38 -0500

    solve stringMatching w.o regex

--- lesson03-strings/problem.js
+++ lesson03-strings/problem.js
@@ -1,9 +1,11 @@
 /*
 Problem:
 
-You are given a text string and a pattern string. Your task is to implement a string matching algorithm that finds all occurrences of the pattern in the text.
+You are given a text string and a pattern string. Your task is to implement a string matching algorithm 
+that finds all occurrences of the pattern in the text.
 
-Write a function `stringMatching(text, pattern)` that takes in two strings as arguments and returns an array of indices where the pattern is found in the text. If the pattern is not found, return an empty array.
+Write a function `stringMatching(text, pattern)` that takes in two strings as arguments and returns an array 
+of indices where the pattern is found in the text. If the pattern is not found, return an empty array.
 
 Example:
 
@@ -12,7 +14,7 @@ text = "ababcababcabc"
 pattern = "abc"
 
 Output:
-[2, 7, 10, 13]
+[2, 7, 10]
 
 Input:
 text = "hello world"
@@ -27,10 +29,21 @@ Note:
 
 */
 
-function stringMatching(text, pattern) {
+const stringMatching = (text, pattern) => {
   // TODO: Implement the string matching algorithm
   // Return an array of indices where the pattern is found in the text
   // If the pattern is not found, return an empty array
+  let indicies = [];
+  let index = text.indexOf(pattern);
+
+  while(index !== -1) {
+    indicies.push(index);
+    index = text.indexOf(pattern, index + 1);
+  }
+
+  return indicies;
 }
+
+console.log(stringMatching("ababcababcabc", "abc"))
 
 module.exports = stringMatching;
