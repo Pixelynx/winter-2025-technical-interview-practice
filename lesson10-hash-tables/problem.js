15be1721aa1fd3d5dc86545c2bb6349b873d3890
pixelynx <tjstevens1992@gmail.com>
2025-02-10 11:47:55 -0500

    update test cases; solutions for warmup and problem

--- lesson10-hash-tables/problem.js
+++ lesson10-hash-tables/problem.js
@@ -7,8 +7,44 @@
  * @param {number} target - The target sum.
  * @return {number[]} - An array containing the indices of the two numbers that add up to the target.
  */
+
+// O(n)^2: CURRENTLY FAILING LAST TEST CASE
+// const twoSum = (nums, target) => {
+//   for(let i = 0; i < nums.length; i++) {
+//     // Loop through the rest of the array to find a pair
+//     for(let j = i + 1; j < nums.length; j++) {
+//       // Check if the two numbers add up to the target
+//       if(nums[i] + nums[j] === target) {
+//         // Return their indices as an array
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
+// HASH MAP SOLUTION
 const twoSum = (nums, target) => {
-  // Your code here
+  let complementMap = new Map();
+  // Find complement between number at index and target
+  // If current number at the index is in the map; return indicies, means we've seen complement
+  // If not found, add it's complement to map
+  for(let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if(complementMap.has(nums[i])) {
+      return [complementMap.get(nums[i]), i];
+    }
+    complementMap.set(complement, i);
+  }
+  // If no complements, return empty array
+  return [];
 };
+
+// const nums = [2, 7, 11, 15]; // => Target:9 | Output: [0, 1]
+const nums = [1, 5, 3, 7]; // Target:8 | Output: [1, 2]
+// const target = 9;
+const target = 8;
+const result = twoSum(nums, target);
+console.log(result);
+
 
 module.exports = twoSum;
